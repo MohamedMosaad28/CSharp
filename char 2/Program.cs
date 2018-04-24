7e0ee6c000162b558b4b8b0fe28c7cc81d1bfb9f
MohamedMosaad28 <38689189+MohamedMosaad28@users.noreply.github.com>
2018-04-24 21:26:42 +0200

    Char 2

---
+++ char 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace char_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvalue = 'M';
+            int myvalue1 = 'm';
+
+            string mystring = "this my character M";
+            string mystring1 = "this my character m";
+
+            Console.WriteLine("{0} value {1} {2} Value {3}", mystring, myvalue, mystring1, myvalue1);
+            Console.ReadKey();
+
+        }
+    }
+}
