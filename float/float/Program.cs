e70e1c05c60dbbacc5f9514ef9e37e2ce037798d
MohamedMosaad28 <38689189+MohamedMosaad28@users.noreply.github.com>
2018-04-24 19:39:54 +0200

    First App

---
+++ float/float/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @float
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flmin = float.MinValue;
+            float flmax = float.MaxValue;
+
+            Console.WriteLine("min Value = {0} max Value = {1}",flmin,flmax);
+
+            float flvar = 1F / 3;
+            double dlvar = 1D / 3;
+            decimal dcvar = 1M / 3;
+
+            Console.WriteLine("\nfloat value = {0} \ndouble value = {1} \ndeciaml value = {2}", flvar, dlvar, dcvar);
+
+            Console.ReadKey();
+        }
+    }
+}
